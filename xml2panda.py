edb4b5a3476b4c296171a644768987a306df525c
aryaman <aryaman4@illinois.edu>
2019-01-02 19:19:20 +0530

    pandas dataframe creation from xml

---
+++ xml2panda.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def xml_panda(file):
+    tree = ET.parse(file)
+    data = dict()
+    attribs = ['id', 'published-at', 'title']
+    data['text'] = list()
+    root = tree.getroot()
+    for i, article in enumerate(root.findall('article')):
+        if len(article.attrib.keys()) != 3:
+            for key in attribs:
+                if key not in article.attrib.keys():
+                    data[key].append("")
+                else:
+                    data[key].append(article.attrib[key])
+        else:
+            for key, value in article.attrib.items():
+                if key not in data.keys():
+                    data[key] = list()
+                    data[key].append(value)
+                else:
+                    data[key].append(value)
+        temp = ""
+        for j, para in enumerate(article.findall('p')):
+            if para.text is None:
+                continue
+            temp += para.text
+        data['text'].append(temp)
+    return pd.DataFrame(data=data)
+
+
+
